e43e646ad85d9e83fe5fa3b8482334cc3c73a258
guih0113 <gui.henriquess13@gmail.com>
2026-01-12 12:43:03 -0300

    .

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,5 +1,5 @@
 import { env } from '@/env'
-import { PrismaClient } from '../../generated/prisma'
+import { PrismaClient } from '@prisma/client'
 
 const clientConfig: ConstructorParameters<typeof PrismaClient>[0] = {
   log: env.NODE_ENV === 'dev' ? ['query'] : [],
